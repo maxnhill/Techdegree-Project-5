26664e69f507314f51bee497cb7de92abab4e2d6
maxnhill <hillmax@umich.edu>
2023-03-02 12:19:10 -0300

    built out app.py

--- app.py
+++ app.py
@@ -1,36 +1,53 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import datetime
+from models import db, Projects, app
+from flask import (render_template, url_for, request, redirect)
 
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pets.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-class Pet(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    created = db.Column('Created',db.DateTime, default = datetime.datetime.now)
-    name = db.Column('Name', db.String())
-    age = db.Column('Age', db.String())
-    breed = db.Column('Breed', db.String())
-    color = db.Column('Color', db.String())
-    size = db.Column('Size', db.String())
-    weight = db.Column('Weight', db.String())
-    url = db.Column('URL', db.String())
-    url_tag= db.Column('URL Tag', db.String())
-    pet_type = db.Column('Pet Type', db.String())
-    gender = db.Column('Gender', db.String())
-    spay = db.Column('Spay', db.String())
-    house_trained = db.Column('House Trained', db.String())
-    description = db.Column('Description', db.Text)
+def add_project():
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        github_url = request.form['github_url']
+        project = Projects(title=title, description=description, github_url=github_url)
+        db.session.add(project)
+        db.session.commit()
+        return redirect(url_for('about.html'))
+    else:
+        return render_template('projectform.html')
 
-    def __repr__(self):
-        return f'''<Pet Name: {self.name} Age: {self.age} 
-                    Breed: {self.breed} Color: {self.color}
-                    Size: {self.size} Weight: {self.weight}
-                    URL: {self.url} Type: {self.pet_type}
-                    Gender: {self.gender} Spay: {self.spay}
-                    House Trained: {self.house_trained}
-                    Description: {self.description}>'''
+@app.route('/pet/<id>')
+def project(id):
+    project = Projects.query.get_or_404(id)
+    return render_template('detail.html', project=project)
+
+@app.route('/edit/<id>', methods=['GET', 'POST'])
+def edit_project(id):
+    project = Projects.query.get_or_404(id)
+    if request.method == 'POST':
+        project.title = request.form['title']
+        project.description = request.form['description']
+        project.github_url = request.form['github_url']
+        db.session.commit()
+        return redirect(url_for('about.html'))
+    else:
+        return render_template('edit_project.html', project=project)
+
+@app.route('/delete/<id>')
+def delete_project(id):
+    project = Projects.query.get_or_404(id)
+    db.session.delete(project)
+    db.session.commit()
+    return redirect(url_for('projects'))
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', msg=error), 404
+
+if __name__ == '__main__':
+    with app.app_context():
+         db.create_all()
+    app.run(debug=True, port=8000, host='0.0.0.0')
 
---
+++ models.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projects.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Projects(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column('Title',db.String())
+    date = db.Column('Date',db.DateTime, default = datetime.datetime.now)
+    description = db.Column('Description', db.String())
+    skills = db.Column('Skills Practiced', db.String())
+    github_url = db.Column('Github Link', db.String())
+ 
+    def __repr__(self):
+        return f'''< Title: {self.title} Date:{self.date} Description: {self.description} 
+                    Skills: {self.skills} Github Link: {self.github_url}>'''
