f9fd54c786da63f9d7bc64bb6838aac6e4370972
maxnhill <hillmax@umich.edu>
2023-03-24 16:10:28 -0400

    Fixing format date function

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def about_me():
 def add_project():
     if request.method == 'POST':
         title = request.form['title']
-        date = request.form['date']
+        date = format_date(request.form['date'])
         skills = request.form['skills']
         description = request.form['desc']
         github_url = request.form['github']
@@ -45,7 +45,7 @@ def edit_project(id):
     if request.method == 'POST':
         project.title = request.form['title']
         project.skills = request.form['skills']
-        project.date = request.form['date']
+        project.date = format_date(request.form['date'])
         project.description = request.form['desc']
         project.github_url = request.form['github']
         db.session.commit()
--- formatdate.py
+++ formatdate.py
@@ -2,10 +2,8 @@ from datetime import datetime
 
 def format_date(date_str):
     date_string = date_str.replace(",", " ")
-    date_obj = datetime.strptime(date_string.strip(), '%B %Y')
-    formatted_date = date_obj.strftime("%B, %Y")
+    date_obj = datetime.strptime(date_string.strip(), '%B, %Y')
+    formatted_date = date_obj.strftime('%Y-%m')
     return (formatted_date)
-
-
 
 
